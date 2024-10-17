430e18c840a909980385744fec8a15dcf69e8f35
clovdeveloper <clovdeveloper@gmail.com>
2024-10-17 16:48:42 +0200

    added axception handler

---
+++ src/main/java/com/clovdev/spring_client_crud/exceptionHandler/AppExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.clovdev.spring_client_crud.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Map<String, String>> handleNoSuchElementException(NoSuchElementException exception) {
+        Map<String, String> response = new HashMap<>();
+        response.put("response_message", exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
